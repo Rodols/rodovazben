b26188c10fbbd211b03dffe5de6f61ac88daace9
Rodo <vazquezbenitezrodofolfo@gmail.com>
2020-12-30 15:13:09 -0600

    Se deja trabajando botones de inicio de pagina web

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {
   trigger,
   state,
@@ -33,7 +33,22 @@ import {
   ],
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('hardware') hardware: ElementRef;
+  @ViewChild('servicios') servicios: ElementRef;
+  @ViewChild('software') software: ElementRef;
   constructor() {}
 
   ngOnInit(): void {}
+
+  serviciosVer() {
+    this.servicios.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  softwareVer() {
+    this.software.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  hardwareVer() {
+    this.hardware.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
+  providers: [HomeComponent],
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private compHome: HomeComponent) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  serviciosVer() {
+    this.compHome.serviciosVer();
   }
-
 }
